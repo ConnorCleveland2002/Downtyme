f8019bf6bf08f8697be5668e691ee1e67d75be31
Mark Andersen <10101087+MarkAndersen@users.noreply.github.com>
2021-06-29 19:53:27 -0400

    first user model/route and rudimentary server setup

---
+++ Controllers/Routes/api/userRoute.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const { User } = require('../../../Models');
+
+//CRUD
+
+router.get('/', async (req, res) => {
+try {
+    const userData = await User.create(req.body);
+    res.status(200).json(userData);
+} catch (err) {
+    res.status(400).json(err);
+}
+});
+// router.post((req, res) => {
+// //create
+// });
+// router.put((req, res) => {
+// //update
+// });
+// router.delete((req, res) => {
+// //delete
+// });
+
+module.exports = router;
