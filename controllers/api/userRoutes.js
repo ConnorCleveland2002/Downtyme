92f25fdf2fe71ba7de29af44ea4f52bb61d2b17a
Mark Andersen <10101087+MarkAndersen@users.noreply.github.com>
2021-07-01 19:20:08 -0400

    updated routes for user login and added paramter to user object

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,24 +1,86 @@
-const router = require('express').Router();
-const { User } = require('../../models');
+const router = require("express").Router();
+const { User } = require("../../models");
 
 //CRUD
+//where are we creating the user?
 
-router.get('/', async (req, res) => {
-try {
-    const userData = await User.create(req.body);
-    res.status(200).json(userData);
-} catch (err) {
+//create
+
+router.post("/", async (req, res) => {
+  try {
+    const userCreate = await User.create(req.body);
+
+    req.session.save(() => {
+      req.session.user_id = userCreate.id;
+      req.session.logged_in = true;
+    });
+    res.status(200).json(userCreate);
+  } catch (err) {
     res.status(400).json(err);
-}
+  }
 });
-// router.post((req, res) => {
-// //create
-// });
-// router.put((req, res) => {
-// //update
-// });
-// router.delete((req, res) => {
-// //delete
-// });
+
+//login
+router.post("/login", async (req, res) => {
+  try {
+    const userLogin = await User.findOne({ where: { email: req.body.email } });
+
+    if (!userLogin) {
+      res
+        .status(400)
+        .json({ message: "Please enter a correct email, or password" });
+      return;
+    }
+
+    const validPassword = await userLogin.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: "Please enter a correct email, or password" });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userLogin.id;
+      req.session.logged_in = true;
+
+      res.json({ user: userLogin, message: "You are now logged in!" });
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+//logout
+
+router.post('/logout', (res, req) => {
+    if (req.session.logged_in) {
+        req.session.destroy(() => {
+            res.status(204).end(); 
+        });
+    } else {
+        res.status(404).end();
+    }
+});
 
 module.exports = router;
+
+
+//delete
+
+// router.delete("/:id", (req, res) => {
+//   try {
+//     const deleteUser = await User.destroy(req.params.id);
+//     if (!deleteUser) {
+//       res.status(404).json({ message: "No user found with this id!" });
+//       return;
+//     }
+//     res.status(200).json(deleteUser);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+
+
