e5608741ac241047d50b53d991d024d03a453228
Connor Cleveland <connorcleveland2002@gmail.com>
2021-07-06 21:16:37 -0400

    Update homeRoutes.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { googleBook } = require('../models');
+const { googleBook, User, Song } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
