14dc4921e5bf1cd3a970344ec2b83fd3e5a2bdf6
Connor Cleveland <connorcleveland2002@gmail.com>
2021-07-07 17:07:13 -0400

    added changed song routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,30 +1,48 @@
 const router = require('express').Router();
-const { googleBook, User, Song } = require('../models');
-const withAuth = require('../utils/auth');
+const sequelize = require('../config/connection');
+const { google_book, User, songs } = require('../models');
 
-router.get('/', async (req, res) => {
+
+router.get('/home', async (req,res) => {
+  res.render('homepage')
+});
+
+router.get('/books', async (req, res) => {
   try {
-        const userData = await User.findAll({
-          include: [{
+    const mediaOne = await google_book.findAll({
+      include: [
+        {
           model: User,
           attributes: ['name'],
         },
       ],
     });
-    const bookData = await googleBook.findAll({
+
+    const dataOne = mediaOne.map((media) => media.get({ plain: true }));
+
+    res.render('homepage', { 
+      dataOne, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+router.get('/songs', async (req, res) => {
+  try {
+    const mediaTwo = await songs.findAll({
       include: [
-        {},
+        {
+          model: User,
+          attributes: ['name'],
+        },
       ],
     });
 
-    // Serialize data so the template can read it
-    const userArr = userData.map((user) => user.get({ plain: true }));
-    const googleBooks = bookData.map((googleBook) => googleBook.get({ plain: true }));
+    const dataTwo = mediaTwo.map((media) => media.get({ plain: true }));
 
-    // Pass serialized data and session flag into template
     res.render('homepage', { 
-      userArr, 
-      googleBooks, 
+      dataTwo, 
       logged_in: req.session.logged_in 
     });
   } catch (err) {
@@ -32,53 +50,36 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/googleBook/:id', async (req, res) => {
-  try {
-    const bookData = await googleBook.findByPk(req.params.id, {
-      include: [
-        {},
-      ],
-    });
 
-    const googleBook = bookData.get({ plain: true });
+router.get('/', (req, res) => {
+    if (req.user) {
+      res.redirect('/home');
+    } else {
+      res.render('signup', {js: ['signup.js']});
+    }
+  });
 
-    res.render('googleBook', {
-      ...googleBook,
-      logged_in: req.session.logged_in
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+  router.get('/login', (req, res) => {
+    if (req.user) {
+      res.redirect('/home');
+    } else {
+      res.render('login', {js: ['login.js']});
+    }
+  });
 
-// Use withAuth middleware to prevent access to route
-router.get('/profile', withAuth, async (req, res) => {
-  try {
-    // Find the logged in user based on the session ID
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [{ model: googleBook }],
-    });
-
-    const user = userData.get({ plain: true });
-
-    res.render('profile', {
-      ...user,
-      logged_in: true
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-router.get('/login', (req, res) => {
-  // If the user is already logged in, redirect the request to another route
-  if (req.session.logged_in) {
-    res.redirect('/profile');
-    return;
-  }
-
-  res.render('login');
-});
+  router.get('/home', (req, res) => {
+    if (req.user)
+    res.render('home', {js: ['home.js']});
+  });
+  router.get('/browse', (req, res) => {
+    if (req.user)
+    res.render('browse', {js: ['media.js']});
+  });
+  
+  router.get('/save', (req, res) => {
+    if (req.user)
+    res.render('save', {js: ['saveMedia.js']});
+  });
+    
 
 module.exports = router;
